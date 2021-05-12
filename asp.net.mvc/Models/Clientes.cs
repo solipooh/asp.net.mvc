22738e8799054b751727670821f435dd195d83fa
Cristian <lic.cristian.sv@gmail.com>
2021-05-11 20:46:22 -0500

    Practica 21 El Modelo y se creo el proyecto

---
+++ asp.net.mvc/Models/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asp.net.mvc.Models
+{
+    public class Clientes
+    {
+        public int ID { get; set; }
+        
+        public string Nombre { get; set; }
+        
+        public DateTime FechaAlta { get; set; }
+        
+        public int Edad { get; set; }
+    }
+}
